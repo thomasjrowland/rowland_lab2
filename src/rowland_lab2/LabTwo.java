7883f1f5f27de3e853c6d4c60f336f8facece1ad
Tom Rowland <thomasjrowland@gmail.com>
2020-04-21 09:52:49 -0400

    initial commit

---
+++ src/rowland_lab2/LabTwo.java
@@ -0,0 +1,51 @@
+package rowland_lab2;
+
+import java.util.Scanner;
+
+public class LabTwo {
+
+	public static void main(String[] args) {
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		boolean keepGoing = true;
+		
+		while (keepGoing) {
+			
+			System.out.print("Please enter the room's length in inches: ");
+		
+			double length = scnr.nextDouble();
+		
+			System.out.print("Please enter the room's width in inches: ");
+		
+			double width = scnr.nextDouble();
+		
+			System.out.print("Please enter the room's height in inches: ");
+				
+			double height = scnr.nextDouble();
+		
+			System.out.println();
+		
+			double roomArea = length * width;
+				
+			double roomVolume = length * width * height;
+		
+			System.out.printf("The area of the room is: %.2f square inches" , roomArea);
+			System.out.println();
+		
+			System.out.printf("The volume of the room is: %.2f square inches" , roomVolume);
+			System.out.println();
+			System.out.println();
+		
+			System.out.print("Would you like to continue? (y/n) " );
+			keepGoing = scnr.next().startsWith("y");
+			System.out.println();
+						
+		}
+		
+		System.out.println();
+		System.out.println("Goodbye!");
+
+	}
+
+}
